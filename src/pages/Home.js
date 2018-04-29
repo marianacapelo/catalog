9f4a83931d7e360da740f31044ce675c697265e0
Mariana Capelo <mcapelo@bsolus.pt>
2018-04-29 12:44:25 +0100

    Home component - first version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,42 @@ import _ from 'lodash';
 
 class Home extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.page = 1;
+        this.pageSize = 5;
+        this.onScroll = this.onScroll.bind(this);
+    }
+
     componentDidMount() {
-        this.props.getPictures();
+        this.props.getPictures(this.page, this.pageSize);
+        window.addEventListener('scroll', _.throttle(this.onScroll, 300), false)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', _.throttle(this.onScroll, 300), false)
+    }
+
+    onScroll() {
+        if (
+          (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
+          this.props.pictures.pictureIds.length
+        ) {
+            this.page++;
+            this.props.getPictures(this.page, this.pageSize);
+        }
     }
 
     render() {
         return (
             <div>
-                Homepage :)
+                <h2>Homepage :)</h2>
+                <div>
+                    { this.props.pictures.pictureIds.length === 0 ? 
+                        'Empty':
+                        this.props.pictures.pictureIds.map((id,idx) => <div key={idx}>Id: {id}</div>)
+                    }
+                </div>
             </div>
         );
     }
